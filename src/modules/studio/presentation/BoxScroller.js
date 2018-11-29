a79fa909b2ed9e4ab9fcc1122995e94d345f1141
JC <user@Mac-client-5.local>
2018-11-29 19:40:26 +0100

    a

--- src/modules/studio/presentation/BoxScroller.js
+++ src/modules/studio/presentation/BoxScroller.js
@@ -30,7 +30,7 @@ export default class BoxScroller extends React.Component {
     const timer = setInterval(() => {
       if (this.state.i > maxScrolls) {
         this.setState({i:0, boxScrollerAnimation: " "})
-        setTimeout(() => {this.setState({i:this.state.i+1, boxScrollerAnimation: "box-scroller-animation-active"})}, 10)
+        setTimeout(() => {this.setState({i:this.state.i+1, boxScrollerAnimation: "box-scroller-animation-active"})}, 50)
       }
       else this.setState({i:this.state.i+1, boxScrollerAnimation: "box-scroller-animation-active"})
     }, 2000)
